99f18a3447c410ec25aa664f2b35026b2fabaf91
anosillus <anosillus@gmail.com>
2022-06-06 04:35:07 +0900

    python version fix

---
+++ algo-method/307/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# File name: <+FILE NAME+>
+# First Edit: 2022-06-06
+# Last Change: 2022-06-06
+
+import bisect,collections,copy,heapq,itertools,math,numpy,string
+import sys
+
+def I(): return int(sys.stdin.readline().rstrip())
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+
+N = I()
+A = LI()
+
+print(A)
+
+
